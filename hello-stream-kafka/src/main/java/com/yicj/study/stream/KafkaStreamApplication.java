7f3b541ca4aaeced91b124a546a2a4db609f1a06
yichengjie <626659321@qq.com>
2023-10-08 18:28:12 +0800

    add kafka stream init

---
+++ hello-stream-kafka/src/main/java/com/yicj/study/stream/KafkaStreamApplication.java
@@ -0,0 +1,15 @@
+package com.yicj.study.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yicj
+ * @date 2023年10月08日 18:26
+ */
+@SpringBootApplication
+public class KafkaStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamApplication.class, args) ;
+    }
+}
