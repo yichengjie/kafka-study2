abf0f4cefd5b606729a779e3b612ddcd10689ff4
易成杰 <626659321@qq.com>
2024-01-21 19:34:21 +0800

    补充测试

--- hdk-kafka-showcase/src/test/java/com/yicj/study/kafka/producer/HelloMessageProducer2Test.java
+++ hdk-kafka-showcase/src/test/java/com/yicj/study/kafka/producer/HelloMessageProducer2Test.java
@@ -7,8 +7,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
-
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
