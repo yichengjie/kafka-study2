c93895341d18bb9b91390c4e4c7b441c384921df
易成杰 <626659321@qq.com>
2024-01-21 19:34:11 +0800

    补充测试

---
+++ hdk-kafka-showcase/src/test/java/com/yicj/study/kafka/producer/HelloMessageProducer2Test.java
@@ -0,0 +1,37 @@
+package com.yicj.study.kafka.producer;
+
+import com.yicj.study.kafka.config.KafkaProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author yicj
+ * @Since 2024/1/21 12:12
+ */
+@Slf4j
+public class HelloMessageProducer2Test {
+
+
+    @Test
+    public void send() throws InterruptedException, ExecutionException {
+        String topicName = "hello-topic" ;
+        //
+        String bootstrapServer = "192.168.99.51:8989" ;
+        Map<String, Object> properties = new KafkaProperties().init(bootstrapServer);
+        DefaultKafkaProducerFactory<Object, Object> producerFactory = new DefaultKafkaProducerFactory<>(properties);
+        //
+        KafkaTemplate<Object, Object> kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        //
+        ListenableFuture<SendResult<Object, Object>> future = kafkaTemplate.send(topicName, "hello world");
+        future.get() ;
+    }
+
+}
